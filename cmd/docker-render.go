146bf1e569aec9c2671fa68a89e6e2c99855e309
Kaan Yagci <kaan.yagci@makepad.fr>
2024-05-07 15:30:19 +0200

    chore: first version of docker render plugin
    Signed-off-by: Kaan Yagci <kaan.yagci@makepad.fr>

--- cmd/docker-render.go
+++ cmd/docker-render.go
@@ -1,32 +1,21 @@
 package main
 
 import (
-	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
+
+	docker_render "github.com/Makepad-fr/docker-render"
 )
 
-type PluginMetadata struct {
-	SchemaVersion    string `json:"SchemaVersion"`
-	Vendor           string `json:"Vendor"`
-	Version          string `json:"Version"`
-	ShortDescription string `json:"ShortDescription"`
-	URL              string `json:"Url"`
-	Experimental     bool   `json:"Experimental"`
-}
-
 func main() {
-	if len(os.Args) > 1 && os.Args[1] == "docker-cli-plugin-metadata" {
-		metadata := PluginMetadata{
-			SchemaVersion:    "0.1.0",
-			Vendor:           "MAKEPAD",
-			Version:          "0.0.1",
-			ShortDescription: "A Helmlike templating engine for Docker",
-			URL:              "https://github.com/Makepad-fr/docker-template",
-			Experimental:     true,
-		}
-		json.NewEncoder(os.Stdout).Encode(metadata)
+	if docker_render.ShowPluginMetaData() {
 		return
 	}
-	fmt.Println("Hello world")
+	err := docker_render.Run()
+	if err != nil {
+		fmt.Println(err)
+		flag.Usage()
+		os.Exit(1)
+	}
 }
---
+++ plugin_metadata.go
@@ -0,0 +1,31 @@
+package docker_render
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type PluginMetadata struct {
+	SchemaVersion    string `json:"SchemaVersion"`
+	Vendor           string `json:"Vendor"`
+	Version          string `json:"Version"`
+	ShortDescription string `json:"ShortDescription"`
+	URL              string `json:"Url"`
+	Experimental     bool   `json:"Experimental"`
+}
+
+func ShowPluginMetaData() bool {
+	if len(os.Args) > 1 && os.Args[1] == "docker-cli-plugin-metadata" {
+		metadata := PluginMetadata{
+			SchemaVersion:    "0.1.0",
+			Vendor:           "MAKEPAD",
+			Version:          "0.0.1",
+			ShortDescription: "A Helmlike templating engine for Docker",
+			URL:              "https://github.com/Makepad-fr/docker-render",
+			Experimental:     true,
+		}
+		json.NewEncoder(os.Stdout).Encode(metadata)
+		return true
+	}
+	return false
+}
